53108f5c0b58997c811b6dc7e8b7bf78952b46bf
Olivier Bretaud <olivierbretaud@hotmail.fr>
2019-01-31 13:49:38 +0100

    add new works

--- src/components/Portfolio/CarouselPortfolio.js
+++ src/components/Portfolio/CarouselPortfolio.js
@@ -12,9 +12,36 @@ const items = [
     altText: 'Bicool',
     title: 'Bicool',
     description: 'Application web pour la réservation de vélos réalisée en équipe',
-    tools: ["Git", "Github", "React", "Bootstrap", "API JC Decaux", "Google Map","méthodologie agile"],
+    tools: ["Webdesign","Git", "Github", "React", "Bootstrap", "API JC Decaux", "Google Map","méthodologie agile"],
     link: 'https://geoffroy72.github.io/',
     github: 'https://github.com/olivierbretaud/nantes-0918-javascript-easybike'
+  },
+  {
+    src: process.env.PUBLIC_URL+'/assets/img/green-pocket.PNG',
+    altText: 'green-pocket',
+    title: 'green-pocket',
+    description: "Quizz sur l'impact de nos téléphones sur l'environnement réalisé lors d'un hackathon de 48h",
+    tools: ["Webdesign","Vue.js", "Bootstrap", "Animations SVG", "Eco-conception", "méthodologie agile"],
+    link: 'http://green-pocket.olivierbretaud.info/',
+    github: 'https://github.com/olivierbretaud/green-pocket'
+  },
+  {
+    src: process.env.PUBLIC_URL+'/assets/img/MoviesAndMe.png',
+    altText: 'Movies and me',
+    title: 'Movies and me',
+    description: 'Application Android pour rechercher des films et gérer une liste de favoris ( projet personnel )',
+    tools: ["React-Native", "Composants Android", "Redux", "API REST"],
+    link: '',
+    github: ''
+  },
+  {
+    src: process.env.PUBLIC_URL+'/assets/img/business-player.png',
+    altText: 'business-player',
+    title: 'business-player',
+    description: "Prototype d'une application web destinée à faciliter l'élaboration de business plans (réalisée en équipe)",
+    tools: ["Proof of concept", "Git", "React", "Redux", "Bootstrap", "méthodologie agile"],
+    link: 'http://business-player.fr/dev/',
+    github: 'https://github.com/olivierbretaud/nantes-0918-javascript-businessPlayer'
   },
   {
     src: process.env.PUBLIC_URL+'/assets/img/un-mome-a-paris.png',
@@ -24,6 +51,15 @@ const items = [
     tools: ["Git","Github", "Bootstrap", "Animations CSS3", "HTML5"],
     link: 'http://un-mome-a-paris.olivierbretaud.info/',
     github: 'https://github.com/olivierbretaud/Un_mome_a_Paris'
+  },
+  {
+    src: process.env.PUBLIC_URL+'/assets/img/i-pad-my-travels.png',
+    altText: 'my-travels',
+    title: 'My travels',
+    description: "Application web permettant de réaliser des carnets de voyages (projet personnel en cours de réalisation)",
+    tools: ["vue.js", "vuetify","leaflet", "nodes.js","Webdesign", "API"],
+    link: '',
+    github: ''
   },
   {
     src: process.env.PUBLIC_URL+'/assets/img/retzvolution.png',
@@ -38,9 +74,9 @@ const items = [
     src: process.env.PUBLIC_URL+'/assets/img/verts-luisants.png',
     altText: 'verts-Luisants',
     title: 'Verts-Luisants',
-    description: "Site Web pour l'association Verts-Luisants (en cours de réalisation)",
+    description: "Site Web pour l'association Verts-Luisants",
     tools: ["PLUXML(CMS)", "CSS3", "HTML5", "Webdesign"],
-    link: '',
+    link: 'http://vertsluisants.fr/',
     github: 'https://github.com/olivierbretaud/Verts-Luisants'
   }
 ];
@@ -100,14 +136,14 @@ export default class CarouselPortfolio extends Component {
             <img className="img-fluid-slide" src={item.src} alt={item.altText} />
           </div>
           <div className="info-box">
-
             <h1 className="text-center txt-green mt-4">{item.title}</h1>
-
-            <p className="text-center txt-grey mb-0">
-              <a href={item.link}><i className="fas fa-desktop m-3 fa-lg"></i></a>
-              <a href={item.link}><i className="fab fa-github m-3 fa-lg"></i></a>
-            </p>
-
+            {item.link !== '' ?
+              <p className="text-center txt-grey mb-0">
+                <a href={item.link} target="_blank"><i className="fas fa-desktop m-2 fa-lg"></i></a>
+                <a href={item.github} target="_blank"><i className="fab fa-github m-2 fa-lg"></i></a>
+              </p>
+            :
+            null }
             <div className="info-item">
               <p className="text-center font-weight-bold txt-grey mt-2 mb-0 info-item-desc">{item.description}</p>
               <p className="text-center">{item.tools.map((tool, index) =>{
@@ -116,9 +152,7 @@ export default class CarouselPortfolio extends Component {
                   )
               })}</p>
             </div>
-
           </div>
-
         </CarouselItem>
       );
     });
